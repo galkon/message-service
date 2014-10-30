e72ee849b59050449f2fc696ca8b8c1b0484a75b
Michael Treacher <m.w.treacher@gmail.com>
2014-10-30 20:30:34 +1100

    Abstracted database config out into a JSON file.

--- messages.go
+++ messages.go
@@ -2,14 +2,28 @@ package main
 
 import (
   "log"
+  "io/ioutil"
+  "fmt"
+  "os"
+  "encoding/json"
+
   "github.com/galkon/messages/resources"
   "github.com/gin-gonic/gin"
   "github.com/jinzhu/gorm"
   _ "github.com/lib/pq"
 )
 
+type Config struct {
+  DatabaseUser string `json:"databaseUser"`
+  DatabaseName string `json:"databaseName"`
+}
+
+var config Config
+
 func main() {
   r := gin.Default()
+
+  LoadConfig()
 
   conversationResource := &resources.ConversationResource{Db: GetDb()}
 
@@ -20,7 +34,8 @@ func main() {
 }
 
 func GetDb() (gorm.DB) {
-  db, err := gorm.Open("postgres", "user=treach3r dbname=message_service sslmode=disable")
+
+  db, err := gorm.Open("postgres", fmt.Sprintf("user=%s dbname=%s sslmode=disable", config.DatabaseUser, config.DatabaseName))
   
   if err != nil {
     log.Fatal(err)
@@ -32,4 +47,15 @@ func GetDb() (gorm.DB) {
   db.LogMode(true)
 
   return db
+}
+
+func LoadConfig() {
+  config_file, e := ioutil.ReadFile("./config/config.json")
+  
+  if e != nil {
+    fmt.Printf("File error: %v\n", e)
+    os.Exit(1)
+  }
+
+  json.Unmarshal(config_file, &config)
 }
